de1c72c654d4288e1c83e85ef98c1d2c73c533e0
limingyi <11111111>
2018-04-03 14:50:24 +0800

    first

---
+++ src/main/java/org/benmu/com/scv/Test.java
@@ -0,0 +1,13 @@
+package org.benmu.com.scv;
+
+import android.util.Log;
+
+/**
+ * Created by Carry on 2018/4/3.
+ */
+
+public class Test {
+    public static void test() {
+        Log.e("Test", "Test>>>>>>>>>>>>>");
+    }
+}
